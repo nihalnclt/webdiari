67fe8c87b04861ff384a62a93e30a3a6d8564314
Nihal <nihalnclt@gmail.com>
2023-11-24 22:06:13 +0530

    bug fix

--- authService/test/setup.ts
+++ authService/test/setup.ts
@@ -1,5 +1,5 @@
-// import mongoose from "mongoose";
-// import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 // declare global {
 //     var signin: () => string[];
