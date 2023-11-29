eefbc8aea54a0b8728d985d6d22c19e9bf49f71e
Nihal <nihalnclt@gmail.com>
2023-11-29 11:00:44 +0530

    frontend home page updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
-        <div className="flex items-center justify-center mt-[10%]">
-            <em>Coming Soon!</em>
+        <div className="flex flex-col items-center justify-center mt-[10%]">
+            <h2 className="text-3xl font-[600]">Coming Soon..!</h2>
+            <span className="font-medium block mt-2">Webdiari By Nihal N</span>
+            <span className="block mt-2 text-sm">nihalnclt@gmail.com</span>
         </div>
     );
 }
