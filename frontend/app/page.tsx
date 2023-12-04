3cda29047435ef5d4c705975cc3991763df61d61
Nihal <nihalnclt@gmail.com>
2023-12-04 23:05:34 +0530

    simple update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
             <h2 className="text-3xl font-[600]">Coming Soon..!</h2>
             <span className="font-medium block mt-2">Webdiari By Nihal N</span>
             <span className="block mt-2 text-sm">nihalnclt@gmail.com</span>
-            <span className="block mt-2 text-sm">+91 7994766524</span>
         </div>
     );
 }
